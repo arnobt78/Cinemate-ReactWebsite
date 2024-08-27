d2b2f55227b6c23b99b2ce930d48e95cc298d822
Arnob Mahmud <arnob_t78@yahoo.com>
2024-08-28 00:39:25 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="https://www.instagram.com/arnob_t78/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/arnob-mahmud-05839655/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/arnob.mahmud/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Facebook</a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/@arnobcorleone8570" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
+            </li>
+            <li>
+                <a href="https://github.com/arnobt78" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
+            </li>    
+          </ul>
+      </footer>
+  )
+}
